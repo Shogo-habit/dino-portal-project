fd36f4d1bd010755479212dd0bc0e243519e3356
Shogo-habit <kosako20200405@gmail.com>
2026-05-30 14:09:51 +0900

    Configure @astrojs/sitemap and add dynamic Google Analytics 4 & Search Console verification support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://dino-portal-project.vercel.app',
+  integrations: [sitemap()],
+});
