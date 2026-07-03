d692e9226d3e632bde83aa077c003fc346ea708a
Shogo-habit <kosako20200405@gmail.com>
2026-07-04 08:44:13 +0900

    chore: add 301 redirects for kamuyasaurus typo URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dino-portal.com',
+  redirects: {
+    '/dictionary/kamuyasaurus/': '/dictionary/kamuysaurus/',
+    '/dictionary/kamuyasaurus': '/dictionary/kamuysaurus/',
+  },
   integrations: [sitemap({
     filter: (page) => page !== 'https://dino-portal.com/my-collection/' && page !== 'https://dino-portal.com/my-collection',
   }), mdx()],
